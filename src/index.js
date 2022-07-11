5b8d70feac0a2b229bbc99632123cc78569c144b
dev.kimreo <dev.kimreo@gmail.com>
2022-07-11 21:42:49 +0900

    vinilla-redux counter

--- src/index.js
+++ src/index.js
@@ -1,25 +1,31 @@
+import {createStore} from "redux";
+
 const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector("span");
+number.innerText = 0;
 
-let count = 0;
+const ADD = "ADD";
+const MINUS = "MINUS";
 
-number.innerText = count;
-
-const updateText = () => {
-    number.innerText = count;
+const countModifier = (count = 0, action) => {
+    console.log(count, action);
+    switch (action.type) {
+        case ADD:
+            return count + 1;
+        case MINUS:
+            return count - 1;
+        default:
+            return count;
+    }
 };
 
-const handleAdd = () => {
-    count = count + 1;
-    updateText();
-}
+const countStore = createStore(countModifier);
+const onChange = () => {
+    number.innerText = countStore.getState();
+};
 
-const handleMinus = () => {
-    count = count - 1;
-    updateText();
-}
+countStore.subscribe(onChange);
 
-add.addEventListener('click', handleAdd);
-minus.addEventListener('click', handleMinus);
-
+add.addEventListener("click", () => countStore.dispatch({type:ADD}));
+minus.addEventListener("click", () => countStore.dispatch({type:MINUS}));
